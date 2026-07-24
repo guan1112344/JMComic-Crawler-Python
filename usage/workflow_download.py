6e62f6c9a8f85f2e5964efa0525790983c30caeb
guan1112344 <125126951+guan1112344@users.noreply.github.com>
2026-07-25 01:07:56 +0800

    fix env trim empty string causing IndexError

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -2,6 +2,7 @@ from jmcomic import *
 import os
 from jmcomic.cl import JmcomicUI
 
+
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 jm_albums = '''
 1212672
@@ -286,7 +287,7 @@ jm_albums = '''
 jm_photos = '''
 '''
 
-def env(name, default, trim=('[]', '"', '')):
+def env(name, default, trim=('[]', '"')):
     import os
     value = os.getenv(name, None)
     if value is None or value == '':
